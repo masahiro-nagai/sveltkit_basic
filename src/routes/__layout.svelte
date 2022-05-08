4e03f406cffbf16a8f64965e3d1d6b6a4e4e4b10
masahiro-nagai <tavernity.so.n.w@gmail.com>
2022-05-08 23:24:56 +0900

    second commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,47 @@
+<div class="header">
+    <div class="nav">
+        <a href="/home">HOME</a>
+        <a href="/about">ABOUT</a>
+        <a href="/blogs">BLOGS</a>
+    </div>
+</div>
+
+<main class="content">
+    <slot></slot>
+</main>
+
+
+<div class="footer">
+    
+
+
+</div>
+
+<style>
+    .nav{
+        display: flex;
+    }
+    .nav a{
+        padding: 2rem
+    }
+    .content{
+        min-height: 300px;
+        padding: 2rem;
+    }
+
+    .header{
+        display:block;
+        background-color: orange;
+        color: black;
+        height: 200px;
+        padding: 2rem;
+    }
+
+    .footer{
+        display:block;
+        background-color: black;
+        color: white;
+        height: 100px;
+        padding: 2rem;
+    }
+</style>
---
+++ src/routes/home/index.svelte
@@ -0,0 +1,1 @@
+<h1>Hi SWEET Home new folder</h1>
